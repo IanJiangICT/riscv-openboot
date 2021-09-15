25e402ece1b4bf73d97b79ece607d23b50cfe9d9
Ian Jiang <ianjiang.ict@gmail.com>
2021-09-15 11:53:17 +0800

    Add typedef for integers

---
+++ include/simple_types.h
@@ -0,0 +1,9 @@
+#ifndef SIMPLE_TYPES_H
+#define SIMPLE_TYPES_H
+
+typedef unsigned char		uint8_t;
+typedef unsigned short		uint16_t;
+typedef unsigned int		uint32_t;
+typedef unsigned long long	uint64_t;
+
+#endif
