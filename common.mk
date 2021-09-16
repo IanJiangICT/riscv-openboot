4fe2a29fbfe0e0efc0f837ad6fc8d0156a49a9ad
Ian Jiang <ianjiang.ict@gmail.com>
2021-09-16 17:39:12 +0800

    Support building with both GNU tool and LLVM

--- common.mk
+++ common.mk
@@ -3,16 +3,29 @@ SRC_DIR = $(TOP_DIR)/src
 BUILD_DIR = $(TOP_DIR)/build
 INC_DIR =  $(TOP_DIR)/include
 
-CFLAGS = -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles -march=rv64g -mabi=lp64d
-CFLAGS += -static
+ifdef LLVM_PATH
+CC = $(LLVM_PATH)/clang
+LD = $(LLVM_PATH)/ld.lld
+DUMP = $(LLVM_PATH)/llvm-objdump
+CFLAGS += --target=riscv64
+CFLAGS += -mno-relax
+else
+GNU_TOOL = riscv64-linux-gnu-
+CC = $(GNU_TOOL)gcc
+LD = $(GNU_TOOL)ld
+DUMP = $(GNU_TOOL)objdump
+CFLAGS += -nostartfiles
+endif
+
+CFLAGS += -mcmodel=medany -fvisibility=hidden -nostdlib
+CFLAGS += -march=rv64g
+CFLAGS += -mabi=lp64d
+CFLAGS += -O0
+
 CFLAGS += -c
+CFLAGS += -static
 CFLAGS += -Wall
 CFLAGS += -I $(INC_DIR)
 CFLAGS += -I $(SRC_DIR)
 
-LDFLAGS = -nostdlib -nostartfiles
-
-CROSS_TOOL = riscv64-linux-gnu-
-CC = $(CROSS_TOOL)gcc
-DUMP = $(CROSS_TOOL)objdump
-
+LDFLAGS = -nostdlib
