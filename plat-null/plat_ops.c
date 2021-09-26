b930e10e497e6c3cd1bd5d748fb0c5c20df4d0d3
Ian Jiang <ianjiang.ict@gmail.com>
2021-09-26 12:43:16 +0800

    plat: Add reading APIs for SD

--- plat-null/plat_ops.c
+++ plat-null/plat_ops.c
@@ -3,6 +3,9 @@ void plat_bc_fix(void) { return; }
 void plat_flash_init(void) { return; }
 void plat_flash_read_byte(unsigned int offset, unsigned char *buf) { return; }
 void plat_flash_read(unsigned int offset, unsigned char *buf, unsigned int size) { return; }
+void plat_sd_init(void) { return; }
+void plat_sd_read_byte(unsigned int offset, unsigned char *buf) { return; }
+void plat_sd_read(unsigned int offset, unsigned char *buf, unsigned int size) { return; }
 void plat_serial_init(void) { return; }
 void plat_serial_put_byte(unsigned char data) { return; }
 void plat_clock_init(void) { return; }
