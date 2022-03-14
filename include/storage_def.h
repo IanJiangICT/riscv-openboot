a876ed9d1926d2a499ee2536f0e37e8ef8d6a3bf
Ian Jiang <ianjiang.ict@gmail.com>
2022-03-14 16:32:12 +0800

    storage: Define SPI instruction codes

---
+++ include/storage_def.h
@@ -0,0 +1,11 @@
+#ifndef STORAGE_DEF_H
+#define STORAGE_DEF_H
+
+/* SPI Instruction Codes */
+#define SPI_INST_READ			0x03
+#define SPI_INST_READ_4BA		0x13
+#define SPI_INST_FAST_READ		0x0B
+#define SPI_INST_FAST_READ_4BA	0x0C
+#define SPI_INST_JEDEC_ID		0x9F
+
+#endif
