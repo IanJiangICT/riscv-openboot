2e27aac240ddea06cfcaf0ca3e20aaac8d76eaeb
Ian Jiang <ianjiang.ict@gmail.com>
2022-04-27 00:05:43 +0800

    bist: Test serial and flash storage in FSBL

--- include/fsbl_func.h
+++ include/fsbl_func.h
@@ -4,7 +4,12 @@
 #ifdef __ASM__
 .globl fsbl_first_print;
 #else
+
 void fsbl_first_print(void);
+#ifdef FSBL_BIST
+void fsbl_run_bist(void);
+#endif
+
 #endif
 
 #endif
--- src/fsbl_func.c
+++ src/fsbl_func.c
@@ -1,6 +1,7 @@
 #include "simple_types.h"
 #include "bootconf.h"
 #include "serial_func.h"
+#include "storage_func.h"
 #include "plat_ops.h"
 #include "plat_def.h"
 
@@ -16,3 +17,14 @@ void fsbl_first_print(void)
 #endif
 	return;
 }
+
+#ifdef FSBL_BIST
+void fsbl_run_bist(void)
+{
+	struct bootconf *bc = (struct bootconf *)PLAT_RAM_BC;
+	if (((bc->work_mode & 0x03) != BC_WORK_MODE_BIST) || ((bc->work_mode & BC_WORK_MODE_BIST_FSBL) != 0)) return;
+	serial_bist();
+	storage_bist();
+	return;
+}
+#endif
