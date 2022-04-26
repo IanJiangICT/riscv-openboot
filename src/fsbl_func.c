519dee8b70ea4f4bb773344d331be9f6c5525693
Ian Jiang <ianjiang.ict@gmail.com>
2022-04-26 23:56:09 +0800

    fsbl: Print FSBL at starting

--- src/fsbl_func.c
+++ src/fsbl_func.c
@@ -1,1 +1,18 @@
-void fsbl_first_print(void) { return; }
+#include "simple_types.h"
+#include "bootconf.h"
+#include "serial_func.h"
+#include "plat_ops.h"
+#include "plat_def.h"
+
+void fsbl_first_print(void)
+{
+	serial_print_byte('F');
+#ifndef PRINT_SIMPLE
+	serial_print_byte('S');
+	serial_print_byte('B');
+	serial_print_byte('L');
+	serial_print_byte('\10');
+	serial_print_byte('\13');
+#endif
+	return;
+}
